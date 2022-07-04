6250f015be299e89d1049348a72a93660cf35e10
Aryan Suman <aryansuman18@gmail.com>
2022-07-04 14:03:00 +0530

    added coverUrl and removed box office collection from tvshows in movieitem

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -28,13 +28,15 @@ export default function MovieItem(props) {
         if (element.title === title) {
           return <div key={title} className="h-screen">
             <div className="flex flex-col items-center md:my-16 mx-auto rounded-lg border shadow-md md:flex-row md:max-w-4xl  border-gray-700 bg-gray-800">
-              <img className="object-cover  rounded-t-lg md:max-h-[566px] md:rounded-none md:rounded-l-lg" src={element.cover_url} alt="" />
+            <a href={element.cover_url} className="md:min-w-[382px]" target="_blank" rel="noreferrer">
+              <img className="object-cover rounded-t-lg md:max-h-[566px] md:rounded-none md:rounded-l-lg" src={element.cover_url} alt="" />
+            </a>
               <div className="flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight  text-white">{element.title}</h5>
                 <p className="mb-1 text-sm text-gray-400"><span className="font-semibold">Phase: </span>{element.phase}</p>
                 <p className="mb-1 text-sm text-gray-400"><span className="font-semibold">Release Date: </span>{element.release_date}</p>
                 <p className="mb-1 font-normal text-gray-400"><span className="font-semibold">Director: </span>{element.directed_by}</p>
-                <p className="mb-3 font-normal text-gray-400"><span className="font-semibold">Box Office Collection: </span>{(parseInt(element.box_office) / 10000000).toFixed(2)} cr$</p>
+                {keyword === "movies"?<p className="mb-3 font-normal text-gray-400"><span className="font-semibold">Box Office Collection: </span>{(parseInt(element.box_office) / 10000000).toFixed(2)} cr$</p>:null}
                 <p className="mb-3 font-normal text-gray-400">{element.overview}</p>
                 <a href={element.trailer_url} className="inline-flex my-2 w-36 items-center py-2 px-3 text-sm font-medium text-center text-white rounded-lg  focus:ring-4 focus:outline-none bg-red-600 hover:bg-red-700 focus:ring-red-800">
                   Watch Trailer
